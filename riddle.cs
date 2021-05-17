6ef68054a6b725f26aad1f96ee4763854732fa6a
adam zbati <adam.zbati2@gmail.com>
2021-05-17 13:38:02 +0200

    changed riddlegame

--- riddle.cs
+++ riddle.cs
@@ -4,25 +4,46 @@ using System.Collections.Generic;
     public class riddle : NumberGuessBooth
     {
 
-      int riddlesvar = 4;
+    //  int riddlesvar = 4;
+      
       public void riddleme()
       {
 
         trues = false;
-        Console.WriteLine("arya bertil conny dalia bor i ett fyravåningsus. vilken våning bor arya på?");
-        Console.WriteLine("bertil och conny bor under dalia och arya bor över dalia.");
+     //   Console.WriteLine("arya bertil conny dalia bor i ett fyravåningsus. vilken våning bor arya på?");
+     //   Console.WriteLine("bertil och conny bor under dalia och arya bor över dalia.");
+
+     Console.WriteLine("Want to destroy a tall building? if so enter 1");
         
           try
           {
-       ;
+       
             while(trues == false)
             {
 
               Stack<int> housestack = new Stack<int>();
+              housestack.Push(1);
               housestack.Push(2);
               housestack.Push(3);
               housestack.Push(4);
+              housestack.Push(5);
+              housestack.Push(6);
+              housestack.Push(7);
 
+              hisguess = Convert.ToInt32(Console.ReadLine());
+
+              if(hisguess == 1)
+              {
+                
+                while(housestack.Peek() != 1)
+                {
+                  Console.Write("Destroyed story ");
+                  Console.WriteLine(housestack.Pop() - 1);
+                }           
+                 
+              }
+              
+              /*
               hisguess = Convert.ToInt32(Console.ReadLine());
 
     
@@ -38,12 +59,16 @@ using System.Collections.Generic;
                 Console.WriteLine("wrong answer try again");
                 trues = false;
              }
-           
+            */
+
+
            }
+
+          
 
           } catch
             {
-              Console.WriteLine("write in the number of your guessed floor not in letters");
+              Console.WriteLine("write in the number not letters");
             }
 
       }
